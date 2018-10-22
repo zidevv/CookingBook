d9d6a07eca9411b536ce83f06cf18e6843ff75c4
zidevv <zidevv@gmail.com>
2018-10-22 15:28:57 +0200

    Dodaj pliki projektów.

---
+++ CookingBook/Recipes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookingBook
+{
+    
+    public class Recipes
+    {
+        public string nameOfDish; 
+    }
+    public class Product
+    {
+        public string name;
+        public double amount;
+        public bool isItem;
+        public string unit;
+        //public string[] unit = { "szt", "g", "ml", "kg", "ml" };
+        //default
+        public Product() { }
+        //is amount
+        public Product(string n,bool b, double am, string uni) { name = n; isItem = b; amount = am; unit = uni; }
+        
+    }
+}
