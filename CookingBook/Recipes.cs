7bea5b0c805f9dbd3024c69e61b57c9aa9626247
zidevv <zidevv@gmail.com>
2018-10-23 11:14:18 +0200

    Klasy Przepis oraz produkt form1 Wyglad aplikacji

--- CookingBook/Recipes.cs
+++ CookingBook/Recipes.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace CookingBook
 {
     
-    public class Recipes
+    public class Recipe
     {
-        public string nameOfDish; 
+        public string nameOfDish;
+        public List<Product> products = new List<Product>();
+        public List<String> stepsOfRecope;
+        public int timeForPreparing;
+        public int levelOfHard;
+        public string category;
+
+        public Recipe() { }
+
+        public void addSteps(string step)
+        {
+            stepsOfRecope.Add(step);
+        }
+
+        public void saveToFile(List<Recipe> recipes)
+        {
+            var json = JsonConvert.SerializeObject(recipes);
+            System.IO.File.WriteAllText("Recipes.txt", json);
+        }
+
+        public List<Recipe> readFromFile(string adress)
+        {
+            List<Recipe> recipes = new List<Recipe>();
+
+            if (System.IO.File.Exists("Recipes.txt"))
+            {
+                if(System.IO.File.ReadAllText("Recipes.txt")!= string.Empty)
+                {
+                    recipes = JsonConvert.DeserializeObject<List<Recipe>>(System.IO.File.ReadAllText("Recipes.txt"));
+                    return  recipes;
+                }
+                else
+                {
+                    return recipes;
+                }
+            }
+            else
+            {
+                return recipes;
+            }
+         }
+
+        public void removeRecipeFromList(Recipe recipeForDelete, List<Recipe> listOfRecipe)
+        {
+            listOfRecipe.Remove(recipeForDelete);
+        }
+
     }
     public class Product
     {
@@ -18,10 +65,15 @@ namespace CookingBook
         public bool isItem;
         public string unit;
         //public string[] unit = { "szt", "g", "ml", "kg", "ml" };
-        //default
-        public Product() { }
-        //is amount
-        public Product(string n,bool b, double am, string uni) { name = n; isItem = b; amount = am; unit = uni; }
         
+        public Product() { }
+        
+        public Product(string n,bool b, double am, string uni) { name = n; isItem = b; amount = am; unit = uni; }
+
+        public void removeProductFromList(List<Product> listOfProduct, Product productForDelete)
+        {
+            listOfProduct.Remove(productForDelete);
+        }
+
     }
 }
